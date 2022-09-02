ff25a56691c3e13eb81f091955d98d3949b67ab6
bellamy.k <bellamy.k@kakaocorp.com>
2022-09-02 17:48:35 +0900

    Delete dark mode colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '../styles/globals.css'
 import MainLayout from "../components/MainLayout";
+import Head from "next/head"
 
 function App({ Component, pageProps }) {
-  return <MainLayout>
-           <Component {...pageProps} />
-         </MainLayout>
+  return(
+    <>
+        <Head>
+            <meta name="color-scheme" content="light only"/>
+        </Head>
+        <MainLayout>
+            <Component {...pageProps} />
+        </MainLayout>
+    </>
+  )
 }
 
 export default App
