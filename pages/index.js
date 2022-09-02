a481a39b29202c84566788238f39be469a9735c6
bellamy.k <bellamy.k@kakaocorp.com>
2022-09-02 16:08:50 +0900

    Fix quote

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 const Homepage = () => {
-    return <><h1> Welcome to Bellamy's blog </h1><h2>Data engineer @kakao corp</h2></>
+    return <><h1> Welcome to Bellamy&apos;s blog </h1><h2>Data engineer @kakao corp</h2></>
 }
 export default Homepage
