13c18d38475e5bd971f86149c81b962c0c825735
bellamy.k <bellamy.k@kakaocorp.com>
2022-10-11 22:11:25 +0900

    Implement Write Page

--- pages/posts/write.js
+++ pages/posts/write.js
@@ -1,7 +1,37 @@
-import {useState} from 'react';
+import {useState, useRef} from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 
 const WritePage = () => {
-    return (<></>)
+    const editorRef = useRef(null);
+    const log = () => {
+        if (editorRef.current) {
+            console.log(editorRef.current.getContent());
+        }
+    };
+    return (
+        <>
+            <Editor
+                apiKey='wpjs3cehtckttfq8b5xatgp8f7awyr7or3fekq6u818ks4ub'
+                onInit={(evt, editor) => editorRef.current = editor}
+                initialValue="<p>This is the initial content of the editor.</p>"
+                init={{
+                    height: 500,
+                    menubar: false,
+                    plugins: [
+                        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                        'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                        'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+                    ],
+                    toolbar: 'undo redo | blocks | ' +
+                        'bold italic forecolor | alignleft aligncenter ' +
+                        'alignright alignjustify | bullist numlist outdent indent | ' +
+                        'removeformat | help',
+                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                }}
+            />
+            <button onClick={log}>Log editor content</button>
+        </>
+    );
 }
 
 export default WritePage
